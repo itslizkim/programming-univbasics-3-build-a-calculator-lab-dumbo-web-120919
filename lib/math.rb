916879cc3ac2fecd8ebe2845668ceddfd4da7c0c
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-11-10 20:55:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+ sum = num1 + num2
+ return result
 end
 
 def subtraction(num1, num2)
-
+ difference = num1 - num2
+ return result
 end
 
 def division(num1, num2)
-
+quotient = num1/num2
+return quotient
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
+return product
 end
 
 def modulo(num1, num2)
@@ -19,5 +23,6 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-
+result = math.sqrt(num)
+return result
 end
